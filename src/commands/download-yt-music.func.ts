19b99678ecdaa7545e17f6a7000b2bbb79aad0c7
codevisionevgen <codevisionevgen@gmail.com>
2025-10-18 23:20:35 +0300

    feat: add validation

--- src/commands/download-yt-music.func.ts
+++ src/commands/download-yt-music.func.ts
@@ -8,15 +8,14 @@ export const handler: ICommandFunc = {
   callback: async (ctx, name) => {
     const downloader = new YTAudioDownloader();
     console.log(name);
-    if (ctx.text) {
-      const song = await downloader.download(ctx.text);
-      console.log(song);
-      if (song)
-        ctx.sendAudio({
-          source: Buffer.from(await song.bytes()),
-          filename: song.name,
-        });
-      // ctx.sendAudio(song.outputFile);
-    }
+    if (!ctx.text || !ctx.text?.startsWith("https://")) return;
+    ctx.reply("run");
+    const song = await downloader.download(ctx.text);
+    console.log(song);
+    if (song)
+      ctx.sendAudio({
+        source: Buffer.from(await song.bytes()),
+        filename: song.name,
+      });
   },
 };
